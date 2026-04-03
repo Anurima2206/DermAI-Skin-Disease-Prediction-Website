ca871d9669b45f44c97e1c8fa9d6e0dcf7582d25
Anurima2206 <anutri2k05@gmail.com>
2026-04-03 21:14:51 +0530

    fixed

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"))
 
-/* ensure uploads folder exists (important for Render deployment) */
-const uploadPath = path.join(__dirname, "uploads");
-
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath);
-}
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
@@ -26,6 +20,5 @@ app.use('/api/v1/predict',require("./routes/predictionRoutes.js"));
 app.use('/api/v1/doctor',require("./routes/doctorRoutes.js"));
 app.use('/api/v1/feedback',require("./routes/feedbackRoutes.js"));
 app.use('/api/v1/medical',require("./routes/medicalRoutes.js"));
-/* serve uploaded images */
-app.use("/uploads", express.static(uploadPath));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 module.exports = app;
